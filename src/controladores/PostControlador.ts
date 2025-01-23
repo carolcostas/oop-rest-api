e63f485773e152200ab1ded6f00192721a4a97f4
Carol <xcarol.costa@outlook.com>
2025-01-23 15:36:48 -0300

    implementação do editar post

--- src/controladores/PostControlador.ts
+++ src/controladores/PostControlador.ts
@@ -46,4 +46,27 @@ export default class PostControlador {
     posts.push(post);
     return res.status(201).json(post);
   }
+  editar(req: Request, res: Response) {
+    const { id } = req.params;
+    const { titulo, descricao } = req.body;
+
+    if (!titulo || !descricao) {
+      return res.status(400).json({
+        mensagem: "O titulo e descrição são obrigatórios",
+      });
+    }
+    const post = posts.find((elemento) => {
+      return elemento.id === id;
+    });
+
+    if (!post) {
+      return res.status(404).json({
+        mensagem: "A postagem não existe!",
+      });
+    }
+    post.titulo = titulo;
+    post.descricao = descricao;
+
+    return res.status(204).send();
+  }
 }
