250409b2abead3602f0629e476dc94a6448a8ee3
Carol <xcarol.costa@outlook.com>
2025-01-23 01:19:28 -0300

    implementação do detalhar autores

--- src/controladores/AutorControlador.ts
+++ src/controladores/AutorControlador.ts
@@ -1,8 +1,24 @@
-import {  Request, Response } from "express";
+import { Request, Response } from "express";
 import { autores } from "../bancodedados";
 
 export default class AutorControlador {
-  listar(req: Request, res: Response){
-     res.json(autores);
-  }
+	listar(req: Request, res: Response){
+		return res.json(autores)
+	}
+
+	detalhar(req: Request, res: Response){
+		const { id } = req.params
+
+		const autor = autores.find((elemento) => {
+			return elemento.id === id
+		})
+
+		if (!autor) {
+			return res.status(404).json({
+				mensagem: 'Autor não encontrado!'
+			})
+		}
+
+		return res.json(autor)
+	}
 }
